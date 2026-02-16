401eb62a9233e5eadacd0e094b5161a43fbe7fd2
Noah Shrader <noahshrader@gmail.com>
2026-02-16 07:39:43 -0500

    Initial Commit

---
+++ NoahAssistant/NoahAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoahAssistantApp.swift
+//  NoahAssistant
+//
+//  Created by Noah Shrader on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct NoahAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
